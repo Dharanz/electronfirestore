c7ea9ec48481687f8861d5bd70679421256efbc0
Murali Dharan.M <33930688+Dharanz@users.noreply.github.com>
2022-09-15 14:48:10 +0530

    Update index.js

--- Electron and FireStore/index.js
+++ Electron and FireStore/index.js
@@ -2,13 +2,13 @@
   import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-firestore.js";
 
   const firebaseConfig = {
-    apiKey: "AIzaSyAKwFiPpnjEVWLUDOdhZwVED4wvyn0zqqU",
+    apiKey: "",
     authDomain: "sample-app-321.firebaseapp.com",
     projectId: "sample-app-321",
     storageBucket: "sample-app-321.appspot.com",
-    messagingSenderId: "849654839705",
-    appId: "1:849654839705:web:a47e2cf3dac5c39e917b08",
-    measurementId: "G-QCFVX20M67"
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
   // Initialize Firebase
